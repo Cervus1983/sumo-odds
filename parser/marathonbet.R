5f3e319e49eac80faebd798ad2f1649d406ba0a2
Cervus1983 <cervus1983@gmail.com>
2018-05-16 15:20:45 +0000

    fail gracefully when no odds

--- parser/marathonbet.R
+++ parser/marathonbet.R
@@ -1,5 +1,6 @@
 library(jsonlite)
 library(rvest)
+library(stringr)
 library(tidyverse)
 
 parse_marathonbet <- function(raw_html) {
@@ -10,11 +11,15 @@ parse_marathonbet <- function(raw_html) {
 		lapply(function(x) fromJSON(x)[c("sn", "epr")])	%>% 
 		map_df(bind_rows)
 	
-	if (is.data.frame(df) && nrow(df) > 0 && nrow(df) %% 2 == 0) cbind(
-		df[c(TRUE, FALSE), 1:2],
-		df[c(FALSE, TRUE), 2:1]
-	) %>% 
-		set_names(c("rikishi1", "odds1", "odds2", "rikishi2")) %>% 
-		mutate_at(vars(contains("odds")), as.numeric) %>% 
-		as_tibble()
+	if (is.data.frame(df) && nrow(df) > 0 && nrow(df) %% 2 == 0 && "sn" %in% names(df)) {
+		df <- df %>% mutate(sn = str_extract(sn, "^[a-zA-Z]+"))
+		
+		cbind(
+			df[c(TRUE, FALSE), 1:2],
+			df[c(FALSE, TRUE), 2:1]
+		) %>% 
+			set_names(c("rikishi1", "odds1", "odds2", "rikishi2")) %>% 
+			mutate_at(vars(contains("odds")), as.numeric) %>% 
+			as_tibble()
+	}
 }
